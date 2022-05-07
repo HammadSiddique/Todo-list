cf0c65f4b3f847b44d213fb514e50f408783ee7e
HammadSiddique <hammad.siddique01@gmail.com>
2022-05-08 01:12:50 +0500

    Add status module

--- src/modules/taskStatus.js
+++ src/modules/taskStatus.js
@@ -1,23 +1,23 @@
 export default class Status {
   completeTask = (array) => {
-    const todoTask = document.querySelectorAll('.todo-task');
-    const checkboxes = document.querySelectorAll('.check-task');
+    const todoTask = document.querySelectorAll(".todo-task");
+    const checkboxes = document.querySelectorAll(".check-task");
     checkboxes.forEach((checkbox, index) => {
-      checkbox.addEventListener('click', () => {
-        todoTask[index].classList.toggle('active');
+      checkbox.addEventListener("click", () => {
+        todoTask[index].classList.toggle("active");
         array[index].isCompleted = !array[index].isCompleted;
-        localStorage.setItem('tasks', JSON.stringify(array));
+        localStorage.setItem("tasks", JSON.stringify(array));
       });
     });
   };
 
   clearCompleted = (tasks) => {
-    const clearBtn = document.querySelector('#clear-items');
-    clearBtn.addEventListener('click', () => {
+    const clearBtn = document.querySelector("#clear-items");
+    clearBtn.addEventListener("click", () => {
       tasks.tasksArray = tasks.tasksArray.filter(
-        (item) => item.isCompleted === false,
+        (item) => item.isCompleted === false
       );
-      localStorage.setItem('tasks', JSON.stringify(tasks.tasksArray));
+      localStorage.setItem("tasks", JSON.stringify(tasks.tasksArray));
       tasks.renderList();
     });
   };
