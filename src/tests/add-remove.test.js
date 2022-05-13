b55404e6a4a7d5209cdef329f84e697a967831ac
HammadSiddique <hammad.siddique01@gmail.com>
2022-05-13 18:13:53 +0500

    Create test for edit tasks function

--- src/tests/add-remove.test.js
+++ src/tests/add-remove.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  *
  */
-import Tasks from '../modules/tasks.js';
+import Tasks from "../modules/tasks.js";
 
 document.body.innerHTML = `<section class="d-flex cols todo-list">
   <header class="header d-flex s-between">
@@ -22,20 +22,27 @@ document.body.innerHTML = `<section class="d-flex cols todo-list">
   </div>
  </section>`;
 
-describe('Testing Add function', () => {
+describe("Testing Add function", () => {
   window.localStorage = Storage.prototype;
   // create add test here
-  test('Test: Add task', () => {
+  test("Test: Add task", () => {
     const todoList = new Tasks();
-    todoList.add('Test');
-    todoList.add('Test 2');
-    todoList.add('test item 3');
+    todoList.add("Test");
+    todoList.add("Test 2");
+    todoList.add("test item 3");
     expect(todoList.tasksArray).toHaveLength(3);
   });
   // create remove test here
-  test('Test: Delete task', () => {
+  test("Test: Delete task", () => {
     const todoList = new Tasks();
     todoList.remove(1);
     expect(todoList.tasksArray).toHaveLength(2);
+  });
+  // create edit test here
+  test("Test: Edit task", () => {
+    const todoList = new Tasks();
+    todoList.update("Test for edit", 1);
+    expect(todoList.tasksArray[0].description).toBe("Test");
+    expect(todoList.tasksArray[1].description).toMatch("Test for edit");
   });
 });
